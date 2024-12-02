3f9717996014e893f6e15b35b9b424e253acf122
mar1k123 <markbaltachi@gmail.com>
2024-12-02 21:05:59 +0300

    update

---
+++ main.py
@@ -0,0 +1,20 @@
+import asyncio
+import logging
+
+from aiogram import Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from handlers import bot, router
+
+
+async def main():
+    dp = Dispatcher(storage=MemoryStorage())
+    dp.include_router(router)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    # database.Base.metadata.create_all(bind=database.engine)
+    asyncio.run(main())
