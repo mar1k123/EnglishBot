26b92cc4d16844ae8755a7f60adf87fb7a96cb25
mar1k123 <markbaltachi@gmail.com>
2026-03-02 16:32:58 +0300

    new

---
+++ bot_instance.py
@@ -0,0 +1,8 @@
+﻿from aiogram import Bot, Router
+
+import config
+
+bot = Bot(token=config.BOT_TOKEN)
+router = Router()
+
+
---
+++ states/bot_states.py
@@ -0,0 +1,28 @@
+﻿from aiogram.fsm.state import State, StatesGroup
+
+
+class QuizStates(StatesGroup):
+    SELECTING_MODE = State()
+    SELECTING_LEVEL = State()
+    ANSWERING = State()
+    CONTEXT_ANSWERING = State()
+    REVERSE_SELECTING_MODE = State()
+    REVERSE_SELECTING_LEVEL = State()
+    REVERSE_ANSWERING = State()
+    right_cnt = "right_cnt"
+    wrong_cnt = "wrong_cnt"
+
+
+class DeleteStates(StatesGroup):
+    waiting_for_word = State()
+
+
+class Reg(StatesGroup):
+    Aword = State()
+    SuggestTranslation = State()
+    Rword = State()
+
+
+class TimerStates(StatesGroup):
+    SETTING_INTERVAL = State()
+    waiting_interval = State()
