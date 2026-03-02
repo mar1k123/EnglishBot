2ba88196cc58dc9716779ffd80be6e215076513c
mar1k123 <markbaltachi@gmail.com>
2026-03-02 16:35:49 +0300

    new

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import logging
 from aiogram import Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
-from handlers import bot, router, reminders_worker
+from handlers import bot, router
+from services.reminder_service import reminders_worker
 
 
 async def main():
     dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(router)
-    reminder_task = asyncio.create_task(reminders_worker())
+    reminder_task = asyncio.create_task(reminders_worker(bot))
     await bot.delete_webhook(drop_pending_updates=True)
     try:
         await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
